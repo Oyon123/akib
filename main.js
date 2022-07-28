fde2db763230b3fd666c8c5380ff84ca6bf07653
Oyon123 <100465013+Oyon123@users.noreply.github.com>
2022-07-28 19:19:04 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 
+/*==================== MENU SHOW Y HIDDEN ====================*/
+var navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
+
+/*===== MENU SHOW =====*/
+navToggle.addEventListener('click', function(){
+  navMenu.classList.add('show-menu');
+})
+/*===== MENU HIDDEN =====*/
+navClose.addEventListener('click', function() {
+  navMenu.classList.remove('show-menu');
+})
+/* Validate if constant exists */
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+/*==================== ACCORDION SKILLS ====================*/
+
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+
+/*==================== SHOW SCROLL UP ====================*/
+
+
+/*==================== DARK LIGHT THEME ====================*/
